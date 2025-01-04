18e13b183b1669d0b7c4ca09ba377f1e9b29a35b
MateusRabelo <mateusaqles@gmail.com>
2025-01-04 03:20:31 -0300

    build: try to build a apk commit

---
+++ android/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tellmemom.app',
+  appName: 'Tell Me Mom',
+  webDir: 'www'
+};
+
+export default config;
